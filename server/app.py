769d98c050b5c66b702cdc9c148b8bc238029971
Fahiye Muhammad <fahiye254@gmail.com>
2025-06-18 15:35:33 +0300

    Cookies and sessions lab intro

--- server/app.py
+++ server/app.py
@@ -1,34 +1,33 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, session, jsonify
 from flask_migrate import Migrate
-
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
-@app.route('/clear')
-def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
-
-@app.route('/articles')
-def index_articles():
-
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+    if session['page_views'] > 3:
+        return jsonify({"message": "Maximum pageview limit reached"}), 401
 
-if __name__ == '__main__':
-    app.run(port=5555)
+    article = Article.query.filter_by(id=id).first()
+    if not article:
+        return jsonify({"error": f"Article with the id {id} not found"}), 404
+
+    return jsonify({
+        'id': article.id,
+        'author': article.author,
+        'title': article.title,
+        'content': article.content,
+        'preview': article.preview,
+        'minutes_to_read': article.minutes_to_read,
+        'date': article.date.isoformat() if article.date else None
+    }), 200
--- server/models.py
+++ server/models.py
@@ -12,25 +12,17 @@ class Article(db.Model, SerializerMixin):
     __tablename__ = 'articles'
 
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String)
+    author = db.Column(db.String)  
     title = db.Column(db.String)
     content = db.Column(db.String)
     preview = db.Column(db.String)
     minutes_to_read = db.Column(db.Integer)
     date = db.Column(db.DateTime, server_default=db.func.now())
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return f'Article {self.id} by {self.author}'
+    serialize_rules = ('-content',)
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
-    articles = db.relationship('Article', backref='user')
-
-    def __repr__(self):
-        return f'User {self.name}, ID {self.id}'
